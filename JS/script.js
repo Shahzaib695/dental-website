4c3f659d2c954976449709341fcf7ac88b4cfe2b
Shahzaib Baig <78021129+Shahzaib695@users.noreply.github.com>
2025-03-11 05:49:08 +0500

    /

--- JS/script.js
+++ JS/script.js
@@ -146,4 +146,21 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".testimonial-swiper", {
+        slidesPerView: 1,  // Adjust number of visible slides if needed
+        spaceBetween: 20, 
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
     
+    
+  });
+  
